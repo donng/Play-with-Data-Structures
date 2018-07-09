44363e784c44d80c089d4ffc16aa1f7a271f1894
donng <xdong001@126.com>
2018-07-09 10:06:04 +0800

    dynamic-array

---
+++ 02-Arrays/07-Dynamic-Array/src/Main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	arr := getArray(10)
+	for i := 0; i < 10; i++ {
+		arr.addLast(i)
+	}
+	fmt.Println(arr)
+
+	arr.add(1, 100)
+	fmt.Println(arr)
+
+	arr.addFirst(-1)
+	fmt.Println(arr)
+}
