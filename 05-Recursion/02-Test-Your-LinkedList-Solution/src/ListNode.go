564c2c7d52cc1f33784aae96f23b38be43702dee
donng <xdong001@126.com>
2018-07-19 14:51:51 +0800

    test-your-linkedlist-solution

---
+++ 05-Recursion/02-Test-Your-LinkedList-Solution/src/ListNode.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+)
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+// 链表节点的构造函数
+// 使用 arr 为参数，创建一个链表，当前的 ListNode 为链表头结点
+
+func GetListNode(arr []int) *ListNode {
+	if arr == nil || len(arr) == 0 {
+		panic("arr can not be empty")
+	}
+
+	head := &ListNode{}
+	head.Val = arr[0]
+
+	prev := head
+	for i := 1; i < len(arr); i++ {
+		prev.Next = &ListNode{Val:arr[i]}
+		prev = prev.Next
+	}
+
+	return head
+}
+
+func (l *ListNode) String() string {
+	var buffer bytes.Buffer
+	for l != nil {
+		buffer.WriteString(fmt.Sprintf("%v ->", l.Val))
+		l = l.Next
+	}
+	buffer.WriteString("NULL")
+	return buffer.String()
+}
---
+++ 05-Recursion/02-Test-Your-LinkedList-Solution/src/Solution.go
@@ -0,0 +1,25 @@
+package main
+
+func removeElements(head *ListNode, val int) *ListNode {
+	for head != nil && head.Val == val {
+		delNode := head;
+		head = head.Next
+		delNode.Next = nil
+	}
+	if head == nil {
+		return nil
+	}
+
+	prev := head
+	for prev.Next != nil {
+		if prev.Next.Val == val {
+			cur := prev.Next
+			prev.Next = cur.Next
+			cur.Next = nil
+		} else {
+			prev = prev.Next
+		}
+	}
+
+	return head
+}
