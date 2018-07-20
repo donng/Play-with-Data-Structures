32a31ff30a0ef3f28fb16069bfee06eaf17e7fe6
donng <xdong001@126.com>
2018-07-20 10:58:46 +0800

    binary-search-tree-basics

---
+++ 06-Binary-Search-Tree/02-Binary-Search-Tree-Basics/src/BST.go
@@ -0,0 +1,28 @@
+package main
+
+type node struct {
+	e interface{}
+	left *node
+	right *node
+}
+
+type BST struct {
+	root *node
+	size int
+}
+
+func GetBST(e interface{}) *BST {
+	bst := &BST{}
+	bst.root = nil
+	bst.size = 0
+
+	return bst
+}
+
+func (t *BST) GetSize() int {
+	return t.size
+}
+
+func (t *BST) IsEmpty() bool {
+	return t.size == 0
+}
