61ba956ddaef7a907ea49da0fc9f99cb3206eb27
donng <xdong001@126.com>
2018-11-02 10:31:38 +0800

    chapter 02-arrays completed

--- 02-Arrays/09-Amortized-Time-Complexity/src/main.go
+++ 02-Arrays/09-Amortized-Time-Complexity/src/main.go
@@ -1,5 +1,36 @@
 package main
 
-func main() {
+import (
+	"Play-with-Data-Structures/02-Arrays/09-Amortized-Time-Complexity/src/Array"
+	"fmt"
+)
 
+func main() {
+	arr := Array.GetArray(10)
+	for i := 0; i < 10; i++ {
+		arr.AddLast(i)
+	}
+	fmt.Println(arr)
+
+	arr.Add(1, 100)
+	fmt.Println(arr)
+
+	arr.AddFirst(-1)
+	fmt.Println(arr)
+
+	arr.Remove(2)
+	fmt.Println(arr)
+
+	arr.RemoveElement(4)
+	fmt.Println(arr)
+
+	arr.RemoveFirst()
+	fmt.Println(arr)
+
+	for i := 0; i < 4; i++ {
+		arr.RemoveFirst()
+		fmt.Println(arr)
+	}
+	// Array: size = 5, capacity = 10
+	// [5, 6, 7, 8, 9]
 }
